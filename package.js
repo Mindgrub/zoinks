5a53ee787f4e1eefcff99c413affb8feba54d5ea
tsibiski <tsibiski@hotmail.com>
2019-05-15 10:14:51 -0400

    Adding package.js

---
+++ package.js
@@ -0,0 +1,43 @@
+{
+  "name": "zoinks",
+  "version": "1.1.2",
+  "_requested": {
+      "type": "tag",
+      "registry": true,
+      "raw": "zoinks",
+      "name": "zoinks",
+      "escapedName": "zoinks",
+      "rawSpec": "",
+      "saveSpec": null,
+      "fetchSpec": "latest"
+  },
+  "_requiredBy": [
+      "#USER"
+  ],
+  "bin": {
+      "zoinks": "zoinks.js"
+  },
+  "bugs": {
+      "url": "https://github.com/Mindgrub/zoinks/issues"
+  },
+  "repository": {
+      "type": "git",
+      "url": "git://github.com/Mindgrub/zoinks.git"
+  },
+  "description": "Crawls through provided website, checking for 200 response, content load, ssl cert errors, and more!",
+  "dependencies": {
+      "puppeteer": "^1.15.0",
+      "minimist": "^1.2.0"
+  },
+  "main": "zoinks.js",
+  "scripts": {
+    "": "node zoinks"
+  },
+  "keywords": [
+    "zoinks",
+    "webcrawler",
+    "test"
+  ],
+  "author": "Tim Sibiski",
+  "license": "GPL-3.0"
+}
